d72d579fd3c69bd752d67c7cd72132e471aea29e
papersaccul <papersacculos@outlook.com>
2026-06-15 18:48:05 +0300

    feat(net): global system proxy

--- hosts/sacculos/local-packages.nix
+++ hosts/sacculos/local-packages.nix
@@ -21,8 +21,5 @@
 
     # Network manager
     networkmanagerapplet
-    
-    # Communication
-    webcord
   ];
 }
--- nixos/modules/net.nix
+++ nixos/modules/net.nix
@@ -4,6 +4,12 @@
   boot.kernel.sysctl."net.ipv4.ip_default_ttl" = 65;
 
   networking = {
+    # proxy all fkn rkn fuck putin
+    proxy = {
+      default = "http://127.0.0.1:7890";
+      noProxy = "127.0.0.1,localhost,internal.domain,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"; # ignore proxy for RFC 1918
+    };
+
     networkmanager = {
       enable = true;
       wifi.powersave = false; # stable connection > battery
@@ -16,7 +22,7 @@
     };
 
     hosts = {
-    "172.66.165.132" = ["api.zed.dev"];
+      "172.66.165.132" = ["api.zed.dev"];
     };
   };
 }
