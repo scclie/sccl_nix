e4eefab6f232150f3d22d398f71eecb45ebc8a86
scclie <contact@sccl.cc>
2026-08-19 17:57:08 +0300

    feat(vesktop): gocryptfs, vesktop as profile module

--- profiles/paper/packages.nix
+++ profiles/paper/packages.nix
@@ -1,35 +1,11 @@
 { config, pkgs, ... }:
 
-
-
-
-let
-  secure-vesktop = pkgs.writeShellScriptBin "vesktop" ''
-    chmod 666 /run/user/1000/''${WAYLAND_DISPLAY:-wayland-0}
-    exec sudo ${pkgs.systemd}/bin/systemd-run \
-      --description="Secure Vesktop (User: paper-dis)" \
-      --property=BindPaths="/var/lib/paper-dis-vesktop:/var/lib/paper-dis-vesktop /run/user/1000/''${WAYLAND_DISPLAY}:/var/lib/paper-dis-vesktop/''${WAYLAND_DISPLAY} /run/user/1000/pulse:/run/user/1000/pulse /run/user/1000/bus:/var/lib/paper-dis-vesktop/bus" \
-      --property=BindReadOnlyPaths=/home/paper:/home/paper \
-      --uid=paper-dis \
-      --gid=paper-dis \
-      --setenv=HOME=/var/lib/paper-dis-vesktop \
-      --setenv=XDG_RUNTIME_DIR=/var/lib/paper-dis-vesktop \
-      --setenv=WAYLAND_DISPLAY=''${WAYLAND_DISPLAY:-wayland-0} \
-      --setenv=DISPLAY=$DISPLAY \
-      --setenv=PULSE_SERVER=unix:/run/user/1000/pulse/native \
-      --setenv=DBUS_SESSION_BUS_ADDRESS=unix:path=/var/lib/paper-dis-vesktop/bus \
-      --wait \
-      ${pkgs.vesktop}/bin/vesktop --no-sandbox --ozone-platform=wayland "$@"
-  '';
-in
 {
   home.packages = with pkgs; [
     # Network
     ayugram-desktop
     chromium
     qbittorrent
-    secure-vesktop
-
     # Crypto
     electrum
     electrum-ltc
