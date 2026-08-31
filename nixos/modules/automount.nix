6ba824fadada3d3139ae8256775ab5f5dbea5dd4
scclie <contact@sccl.cc>
2026-08-31 05:37:25 +0300

    fix(automount): polkit rules

--- nixos/modules/automount.nix
+++ nixos/modules/automount.nix
@@ -5,5 +5,23 @@ in {
     services.udisks2.enable = true;
     security.polkit.enable = true;
     services.gvfs.enable = true;
+
+    # Allow the primary user to mount/unmount drives via udisks2
+    # w/o an admin password prompt
+    security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if ((action.id == "org.freedesktop.udisks2.filesystem-mount-system" ||
+             action.id == "org.freedesktop.udisks2.filesystem-mount" ||
+             action.id == "org.freedesktop.udisks2.filesystem-mount-other-seat" ||
+             action.id == "org.freedesktop.udisks2.filesystem-unmount-others" ||
+             action.id == "org.freedesktop.udisks2.filesystem-unmount" ||
+             action.id == "org.freedesktop.udisks2.eject-media" ||
+             action.id == "org.freedesktop.udisks2.power-off-drive" ||
+             action.id == "org.freedesktop.udisks2.power-off-drive-other-seat") &&
+            subject.isInGroup("wheel")) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
   };
 }
