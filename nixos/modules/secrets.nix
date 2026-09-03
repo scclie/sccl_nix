d7a562c987d0e7f85e21ddb375e1680d61ae52b5
scclie <contact@sccl.cc>
2026-09-03 19:30:00 +0300

    chore(secrets): sops encryption setup + encrypted secrets (infra, db, apps)

--- nixos/modules/secrets.nix
+++ nixos/modules/secrets.nix
@@ -1,40 +1,52 @@
 { config, lib, pkgs, inputs, ... }:
 let cfg = config.sccl.secrets;
 in {
-  options.sccl.secrets.ageKeyFile = lib.mkOption {
-    type = lib.types.path;
-    default = "/var/lib/sops-nix/key.txt";
-    description = "Path to age private key";
+  options.sccl.secrets = {
+    ageKeyFile = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/sops-nix/key.txt";
+      description = "Path to age private key (fallback if sshKeyPaths not available)";
+    };
+    user = lib.mkOption {
+      type = lib.types.str;
+      default = "paper";
+      description = "Owner user for deployed secrets (SSH keys, GPG)";
+    };
   };
 
   imports = [ inputs.sops-nix.nixosModules.sops ];
 
   config = lib.mkIf cfg.enable {
     sops = {
-      age.keyFile = toString cfg.ageKeyFile;
+      age = {
+        # Auto-detect age key from SSH host key (article recommendation)
+        sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+        # Fallback to explicit key file
+        keyFile = toString cfg.ageKeyFile;
+      };
       defaultSopsFile = ../../secrets/common.yaml;
       secrets = {
         "ssh/id_ed25519" = {
-          path = "/home/paper/.ssh/id_ed25519";
-          owner = "paper";
+          path = "/home/${cfg.user}/.ssh/id_ed25519";
+          owner = cfg.user;
         };
         "ssh/id_ed25519_git" = {
-          path = "/home/paper/.ssh/id_ed25519_git";
-          owner = "paper";
+          path = "/home/${cfg.user}/.ssh/id_ed25519_git";
+          owner = cfg.user;
         };
         "ssh/id_ed25519_scclie" = {
-          path = "/home/paper/.ssh/id_ed25519_scclie";
-          owner = "paper";
+          path = "/home/${cfg.user}/.ssh/id_ed25519_scclie";
+          owner = cfg.user;
         };
         "gpg/signing_key" = {
-          path = "/home/paper/.ssh/gpg_signing_key.asc";
-          owner = "paper";
+          path = "/home/${cfg.user}/.ssh/gpg_signing_key.asc";
+          owner = cfg.user;
         };
       };
     };
 
     system.activationScripts.importGpgKey = ''
-      runuser -u paper -- ${pkgs.gnupg}/bin/gpg --import /home/paper/.ssh/gpg_signing_key.asc
+      runuser -u ${cfg.user} -- ${pkgs.gnupg}/bin/gpg --import /home/${cfg.user}/.ssh/gpg_signing_key.asc
     '';
   };
 }
